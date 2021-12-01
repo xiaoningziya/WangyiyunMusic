12a9f37ef563adc87299fb490eeb1e08b03d659e
liukangning <liukangning@kingsoft.com>
2021-12-01 17:49:10 +0800

    更改

--- src/views/player/index.vue
+++ src/views/player/index.vue
@@ -44,7 +44,7 @@ export default defineComponent({
       lyric: '',
       logo: ''
     })
-    const showLyric = ref(false)
+    const showLyric = ref(true)
 
     onMounted(() => {
       songInfo.songId = route.query.songId as string
